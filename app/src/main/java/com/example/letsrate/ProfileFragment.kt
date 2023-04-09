ebed6a8cdaed475e44a3571dff69fd7ea075796c
Niyazi Dinç <devnyzdnc@gmail.com>
2023-04-09 06:07:01 +0300

    firebase connected

--- app/src/main/java/com/example/letsrate/ProfileFragment.kt
+++ app/src/main/java/com/example/letsrate/ProfileFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -20,5 +23,10 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
-    
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
 }
